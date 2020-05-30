e4c7d06de341d44ebaa1c7ab9469204a131b558d
Flexxbo <flexxbo@gmail.com>
2020-05-30 13:05:54 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from "react";
+import { Switch, Route, NavLink } from "react-router-dom";
 import "./App.css";
 import Maincomponent from "./components/Maincomponent";
 import Captchacomponent2 from "./components/Captchacomponent2";
 import Capimg from "./components/Capimg";
+import Introcomponent from "./components/Introcomponent";
+import Component1 from "./components/Component1";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <NavLink to="/"> HomePage </NavLink>
+        <NavLink to="/Step1"> Step1 </NavLink>
+        <NavLink to="/Step2"> Step2 </NavLink>
+        <NavLink to="/Step3"> Step3 </NavLink>
+        <NavLink to="/Step4"> Step4 </NavLink>
+        <NavLink to="/Step5"> Step5 </NavLink>
+      </nav>
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route path="/Step1">
+          <Introcomponent />
+        </Route>
+        <Route path="/Step2">
+          <Component1 />
+        </Route>
+        <Route path="/Step3">
+          <Captchacomponent2 />
+        </Route>
+        <Route path="/Step4">
+          <Capimg />
+        </Route>
+        <Route path="/Step5">
+          <Captchacomponent2 />
+        </Route>
+      </Switch>
       <header className="App-header">
         <h2>Bot-Checker</h2>
-        <Captchacomponent2 />
       </header>
-      <Maincomponent />
-      <Capimg />
     </div>
   );
 }
---
+++ src/components/Component1.js
@@ -0,0 +1,31 @@
+/******************************************
+ *  Author : Flexxbo
+ *  Created On : Sat May 30 2020
+ *  File : Introcomponent.js
+ *******************************************/
+
+/*===Put imports here===*/
+import React from "react";
+
+/*===Start code here===*/
+
+function Component1() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Bot-Checker</h2>
+      </header>
+    </div>
+  );
+}
+
+export default Component1;
+
+/* ===Better Comments=== */
+// "* Usage - Comment everything from the beginning, start a task by adding a comment, then work on task
+// "! Alert - Whenever you need an alert
+// "? Review - Look at this again
+// "TODO: "This ..."  is still to do
+// "//commented out
+// "+ Highlight: This needs to be highlited for some reason
+// "@param Explain Parameters
